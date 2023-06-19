3f54bfc9737bcbbbaf46ec94f6a492df7ac530d0
Mark Brightman <mark.brightman@ravelin.com>
2023-06-19 13:30:08 +0100

    update framework to v2

---
+++ RavelinCore.xcframework/ios-arm64_x86_64-simulator/RavelinCore.framework/Headers/RavelinCore.h
@@ -0,0 +1,18 @@
+//
+//  RavelinCore.h
+//  RavelinCore
+//
+//  Created by Mark Brightman on 13/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RavelinCore.
+FOUNDATION_EXPORT double RavelinCoreVersionNumber;
+
+//! Project version string for RavelinCore.
+FOUNDATION_EXPORT const unsigned char RavelinCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RavelinCore/PublicHeader.h>
+
+
